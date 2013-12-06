733fccf4e34047a1caa0c05530f9938c3be1a887
Andy Russell <arussell123@gmail.com>
2013-12-05 20:20:50 -0800

    add db interface

---
+++ db_interface.py
@@ -0,0 +1,25 @@
+from pony.orm import Database, Required, Set, commit, select
+
+db = Database('sqlite', 'database.sqlite', create_db=True)
+
+class Image(db.Entity):
+    image_url = Required(unicode)
+    post_url = Required(unicode)
+    description = Required(unicode)
+    tags = Set("ImageTag")
+
+class ImageTag(db.Entity):
+    tag = Required(unicode)
+    image = Required(Image)
+
+def store_image(image_url, post_url, description, tags):
+    image = Image(image_url=image_url, post_url=post_url,
+                  description=description)
+    for tag in tags:
+        ImageTag(tag=tag, image=image)
+    commit()
+
+def get_images_for_tag(tag):
+    return select(i for i in Image if tag in i.tags)
+
+db.generate_mapping(create_tables=True)
