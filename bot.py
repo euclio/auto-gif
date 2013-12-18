c901a024f7f0a35baddff589d5969734e2cdf391
Lukesed <lukesed@gmail.com>
2013-12-18 17:30:32 -0500

    adding gensim code. need to test this elsewhere

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import praw
 import requests
 import db_interface
 from bs4 import BeautifulSoup
+from gensim import corpora, models, similarities
 
 r = praw.Reddit(user_agent='Auto-gif: Attempts to respond to comments with relevant '
                 'reaction gifs')
@@ -17,13 +18,61 @@ def login():
         r.login()
 
 
-def reddit_test():
+# Get a list of threads from the top stories on reddit.
+# Each thread is a list of (body, name) pairs.
+def reddit_threads():
     login()
     reactiongifs = r.get_subreddit('reactiongifs')
-    top = reactiongifs.get_top(limit=1).next()
-    comments = top.comments
-    print top
-    print comments[0]
+    threads = []
+    for story in reactiongifs.get_top(limit=10):
+        comments = story.comments
+        print story
+        print len(comments)
+        threads += [comment_descendants(comment) for comment in comments
+                    if type(comment) is praw.objects.Comment]
+    print 'Number of threads is ' + str(len(threads))
+    return threads
+
+
+# Get a list of topics from reddit comments using LDA
+def reddit_topics():
+    threads = []
+    for thread in reddit_threads():
+        text = ''
+        for comment in thread:
+            text += ' ' + comment.body
+        threads.append([word for word in text.lower().split()])
+    print threads
+    all_tokens = []
+    for thread in threads:
+        all_tokens += thread
+    unique = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    common = set(['for', 'a', 'of', 'the', 'and', 'to', 'in', 'http', 'gif',
+                  'mrw'])
+    threads = [[word for word in thread if word not in unique + common]
+               for thread in threads]
+    print threads
+    dictionary = corpora.Dictionary(threads)
+    print dictionary
+    dictionary.save('/tmp/top10.dict')
+    corpus = [dictionary.doc2bow(text) for thread in threads]
+    corpora.MmCorpus.serialize('/tmp/top10.mm', corpus)
+    print corpus
+    model = models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=50)
+    print model
+    return None
+
+
+# Get the text and id of a comment and its descendants, taking the first
+# reply at each level.
+def comment_descendants(comment):
+    descendants = [comment.body, comment.name]
+    while (len(comment.replies) > 0
+           and type(comment.replies[0]) is praw.objects.Comment
+           and comment.replies[0].ups - comment.replies[0].downs > 1):
+        comment = comment.replies[0]
+        descendants.append((comment.body, comment.name))
+    return descendants
 
 
 def scrape():
@@ -38,15 +87,8 @@ def scrape():
         image_url = post.find(class_="middle").find(class_="entry").a["href"]
         title = post.find(class_="middle").find(class_="title").a["title"]
         tags = post.find(class_="post-category").text[6:].split(', ')
-        print title
-        print post_url
-        print image_url
-        print tags
         db_interface.store_image(image_url, post_url, title, tags)
 
+
 if __name__ == '__main__':
-    # only need to scrape once
-    scrape()
-    # see if i can retrieve
-    result = db_interface.get_images_for_tag("mad")
-    print result
+    reddit_topics()
