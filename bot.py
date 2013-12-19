527198bf854edd450e38cd83db508b35da601f9a
Luke Sedney <lukesedney@gmail.com>
2013-12-19 18:24:08 -0500

    look at new comments and find matches

--- bot.py
+++ bot.py
@@ -30,16 +30,16 @@ def login():
         r.login()
 
 
-def days_old(story):
-    '''Return true if story is at least two days old.'''
-    return datetime.now() - datetime.fromtimestamp(story.created) > timedelta(days=2)
-
-
 def reddit_threads(number):
-    """Get a list of threads from the top stories on reddit.
+    """Get a list of old threads from the top stories on reddit.
 
     Each thread is a list of (body, name) pairs.
     """
+
+    def days_old(story):
+        '''Return true if story is at least two days old.'''
+        return datetime.now() - datetime.fromtimestamp(story.created) > timedelta(days=2)
+
     login()
     subr = r.get_subreddit('funny')
     all_threads = []
@@ -49,21 +49,46 @@ def reddit_threads(number):
         print len(comments)
         story_threads = [comment_descendants(comment) for comment in comments if type(comment) is praw.objects.Comment]
         all_threads = chain(all_threads, story_threads)
-    return all_threads
+    return all_threads  
 
 
-def reddit_corpus(name, input_threads):
-    """Save a corpus of reddit comments."""
+def recent_threads(number):
+    """Get a list of old threads from the top stories on reddit.
 
-    def thread_words(thread):
-        text = ''
-        for comment in thread:
-            # Remove punctuation
-            comment = re.sub('[!.:?,;"]', '', comment[0])
-            text += comment
-        return [stem(word) for word in text.lower().split()]
+    Each thread is a list of (body, name) pairs.
+    """
 
-    threads = [thread_words(thread) for thread in input_threads]
+    def hours_old(story):
+        '''Return true if story is under six hours old.'''
+        return datetime.now() - datetime.fromtimestamp(story.created) < timedelta(hours=6)
+
+    login()
+    subr = r.get_subreddit('funny')
+    all_threads = []
+    for story in filter(hours_old, subr.get_top(limit=number)):
+        comments = story.comments
+        print story
+        print len(comments)
+        story_threads = [comment_descendants(comment) for comment in comments if type(comment) is praw.objects.Comment and comment.ups > 5]
+        all_threads = chain(all_threads, story_threads)
+    return all_threads  
+
+
+def thread_words(thread):
+    """Get the words from a list of comments."""
+    
+    text = ''
+    for comment in thread:
+        # Remove punctuation
+        comment = re.sub('[!.:?,;"]', '', comment[0])
+        text += comment
+    return [stem(word) for word in text.lower().split()]
+
+
+def reddit_corpus(name, training_threads):
+    """Save a corpus of reddit threads."""
+
+    threads = [thread_words(thread) for thread in training_threads]
     dictionary = corpora.Dictionary(threads)
     unique_id = [word for word, count in dictionary.dfs.iteritems() if count == 1]
     common_id = []
@@ -81,13 +106,38 @@ def reddit_corpus(name, input_threads):
     corpora.MmCorpus.serialize(name + '.mm', corpus)
 
 
+def classify_new(model, input_threads):
+    """Test a set of new reddit threads with an LDA model and find the comment with the best match."""
+
+    # leave out threads with under 50 characters
+    input_threads = [thread for thread in input_threads if reduce(lambda x,y: x + len(y[0]), thread, 0) > 100]
+
+    threads = [thread_words(thread) for thread in input_threads]
+
+    dictionary = model.id2word
+
+    weighted = []
+    for processed, raw in zip(threads, input_threads):
+        # sort the topics for this thread by weight
+        topics_weights = sorted(model[dictionary.doc2bow(processed)], key = lambda x: -x[1])
+        weighted.append((topics_weights, raw))
+
+    sorted_weighted = sorted(weighted, key = lambda x: -x[0][0][1])
+
+    for topics_weights, comment in sorted_weighted[:3]:
+        best_topic, best_weight = topics_weights[0]
+        print best_topic, best_weight, comment
+        print model.show_topics(topics=-1)[best_topic]
+
+
 def LDA_model(name, number):
     """Create an LDA model from saved corpus and dictionary."""
     corpus = corpora.MmCorpus(name + '.mm')
     print corpus
     dictionary = corpora.Dictionary.load(name + '.dict')
     print dictionary
-    model = models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=number, passes = 10)
+    model = models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=number, passes=10)
+    model.save(name + '.lda')
     print model
     for topic in model.show_topics(topics=-1):
         print topic
@@ -133,7 +183,11 @@ def scrape():
 
 if __name__ == '__main__':
     #scrape()
-    number = 100
+    number = 200
     name = 'top'+ str(number)
-    reddit_corpus(name, reddit_threads(number))
-    LDA_model(name, 200)
+    #reddit_corpus(name, reddit_threads(number))
+    #model = LDA_model(name, 100)
+    model = models.ldamodel.LdaModel.load(name + '.lda')
+    recent_threads = recent_threads(1)
+    classify_new(model, recent_threads)
+
